f551207ee3a046f0b9ea718cbb3214659cff428a
safa2090 <safadridi1987@gmail.com>
2023-09-10 17:40:31 +0100

    .

---
+++ src/Components/PersonCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const PersonCard = (props) => {
+
+    const {firstName, lastName, age, hairColor} = props;
+    return (
+        <div>
+            <h1>
+            {firstName}, {lastName}
+            </h1>
+            <p>Age: {age}</p>
+            <p>HairColor: {hairColor}</p>
+        </div>
+    )
+}
+
+export default PersonCard;
